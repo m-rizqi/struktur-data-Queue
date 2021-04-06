67cb99a49632d0b0e7de016e527e8eae4cd33b4d
Muhammad Rizqi <mrizqi070502@gmail.com>
2021-04-06 09:08:30 +0700

    template method

--- src/Queue.java
+++ src/Queue.java
@@ -1,3 +1,18 @@
 public class Queue {
+    private int rear;
+
+    public int getRear(){
+        return this.rear;
+    }
+
+    public void enQueue(){}
     
+    public void deQueue(){}
+
+    public void isEmpty(){}
+
+    public void isFull(){}
+    
+    public void clear(){}
+
 }
