b49cfc8215eb1dbb3629e50e7ff781cb0caa1d3a
Muhammad Rizqi <mrizqi070502@gmail.com>
2021-04-06 10:03:49 +0700

    add method menu and show

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,64 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
-    }
-    
+        Queue queue = new Queue();
+        Scanner input = new Scanner(System.in);
+        boolean exit = true;
+		while (exit) {
+			menu();
+			System.out.print("Menu yang dipilih : ");
+			int in = input.nextInt();
+			switch (in) {
+				case 1:
+					System.out.println("\n=========================");
+					System.out.println("| T A M B A H   D A T A |");
+                    System.out.println("| Masukkan data :       |");
+					break;
+			
+				case 2:
+					System.out.println("\n=========================");
+					System.out.println("|  A M B I L   D A T A  |");
+					break;
+			
+				case 3:
+					System.out.println("\n=========================");
+					System.out.println("| D A T A  P E R T A M A |");
+					break;
+			
+				case 4:
+					System.out.println("\n=========================");
+					System.out.println("|  S E M U A   D A T A  |");
+                    queue.show();
+					break;
+			
+				case 5:
+					System.out.println("\n=========================");
+					System.out.println("|   B U A T   B A R U   |");
+					break;
+			
+				case 6:
+					exit = false;
+					break;
+			
+				default:
+					System.err.println("\n<-- input salah -->");
+					break;
+			}
+		}
+	}
+
+	private static void menu(){
+		System.out.println("=========================");
+		System.out.println("|        M E N U        |");
+		System.out.println("=========================");
+		System.out.println("|1.| Tambah Data        |");
+		System.out.println("|2.| Ambil Data         |");
+		System.out.println("|3.| Lihat Data Pertama |");
+		System.out.println("|4.| Lihat Semua Data   |");
+		System.out.println("|5.| Buat Queue Baru    |");
+		System.out.println("|6.| Keluar             |");
+	}
+
 }
--- src/Queue.java
+++ src/Queue.java
@@ -1,18 +1,49 @@
 public class Queue {
     private int rear;
+    private int queue[] = new int[10];
 
-    public int getRear(){
-        return this.rear;
-    }
+    public Queue() {
+		this.rear = 4;
+		queue[0] = 3;
+		queue[1] = 6;
+		queue[2] = 5;
+		queue[3] = 2;
+		queue[4] = 70;
+	}
 
     public void enQueue(){}
     
     public void deQueue(){}
-
+    
     public void isEmpty(){}
-
+    
     public void isFull(){}
     
     public void clear(){}
 
+    public void show(){
+		System.out.println();
+        for (int i = 0; i < this.rear; i++) {
+            System.out.print("|‾‾‾‾‾|  ");
+        }
+        System.out.println();
+		for (int i = 0; i < this.rear-1; i++) {
+			System.out.printf("| %- 3d |——",queue[i]);
+		}
+        System.out.printf("| %- 3d |",queue[this.rear-1]);
+        System.out.println();
+        for (int i = 0; i < this.rear; i++) {
+            System.out.print("|_____|  ");
+        }
+		System.out.println();
+		System.out.println();
+	}
+    
+    public int getRear(){
+        return this.rear;
+    }
+    
+    public int[] getQueue(){
+        return this.queue;
+    }
 }
